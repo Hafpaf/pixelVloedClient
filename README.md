b34324468a0b6f7265923f29923d666a7560b022
Hafnium <haf@hafnium.me>
2020-11-04 01:20:43 +0100

    add implementation of protocol 0

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # PixelVloed Client
 
-This client is intended to work with [https://github.com/JanKlopper/pixelvloed](https://github.com/JanKlopper/pixelvloed).
+This client is intended to introduce new users to pixelflut and work with the [PixelVloed C server](https://github.com/JanKlopper/pixelvloed). It should run on anything that takes Python but have only been tested on an X86_64 Linux machine.
 
 ## Setup
-- Set IP and port to what matches the server:
+* **Use cable**, an ethernet connection is faster, more stable and more fun than using a WiFi connection. A bonus is also that other people won't kill you for slowing down their WiFi.
+
+* Inside `client.py`, set IP and port to match the server:
 ```python
-IP = "x.x.x.x"
+IP = "xxx.xxx.xxx.xxx"
 PORT = 5005
+```
+
+## Run client
+```python
+python client.py
+``` 
+or 
+```python
+./client.py
 ```
--- client.py
+++ client.py
@@ -17,39 +17,82 @@ import socket
 import random
 from struct import pack
 
-IP = "ipAddress"
+IP = "10.42.1.12"
 PORT = 5005
 
 # Specifies UDP usage
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 # Color box size
-width, height = 500,500
+width, height = 200,200
+
 # Color box offset
 xoff, yoff = 0, 0
-# Choose a color
-pixelColor = (random.randint(0,255), # R
-              random.randint(0,255), # G
-              random.randint(0,255)) # B
 
+# ----------
+# Start here
+# ----------
+def pixelColor():
+    return (random.randint(0,255), # R
+            random.randint(0,255), # G
+            random.randint(0,255)) # B
+choosenColor = pixelColor()
+
+# -----------------
+# Protocol 0  logic
+# -----------------
 def to_coord(num):
     return pack('BB', num & 0xFF, num >> 8)
 
-def packet(x, y, r, g, b):
-    result = bytes([0, 0])
-    result += bytes([*to_coord(x), *to_coord(y), r, g, b])
-    return result
+pixel_count = 0
+current_packet = None
 
-def imagepacket(x, y):
-    r, g, b = pixelColor
-    result = packet(x + xoff, y + yoff, r, g, b)
-    return result
+def new_packet():
+    global current_packet 
+    global pixel_count
+    current_packet = bytes([0, 0]) #protocol header
+    pixel_count = 0
+new_packet()
 
-packets = [imagepacket(x, y)
-           for x in range(width)
-           for y in range(height)]
-random.shuffle(packets)
+def finish_packet(packets):
+    if pixel_count > 0:
+        packets.append(current_packet)
+        new_packet()
 
+def set_pixel_raw(packets, x, y, r, g, b):
+    global current_packet 
+    global pixel_count
+    current_packet += bytes([*to_coord(x), *to_coord(y), r, g, b])
+    pixel_count +=1
+    if pixel_count >= 160: #160 is max pixel/packet
+        finish_packet(packets)
+
+def set_pixel(packets, x, y, c):
+    r, g, b = c[0], c[1], c[2]
+    set_pixel_raw(packets, x + xoff, y + yoff, r, g, b)
+
+def build_img(packets):
+    for x in range(width):
+        for y in range(height):
+            #cpixel = pixels[x, y]
+            set_pixel(packets, x, y, choosenColor)
+    finish_packet(packets)
+
+print("Build have finished, starting to send UDP")
 while True:
-    for p in packets:
-        sock.sendto(p, (IP, PORT))
+    packets=[]
+    build_img(packets)
+    # makes really nice fading effect:
+    random.shuffle(packets)
+    for n in range(1,25):
+        #print(n)
+        for p in packets:
+            sock.sendto(p, (IP, PORT))
+    xoff += 100
+    yoff += 100
+    xmax = xoff+width
+    ymax = yoff+height
+    if xmax > 1600:
+        xoff = 0
+    if ymax > 1200:
+        yoff = 0
